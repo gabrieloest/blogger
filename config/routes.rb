be1183ef9bf38e15199a3fbeb88d0fe0dc4eaf17
Gabriel Oest <bieloest@gmail.com>
2015-10-09 11:36:07 -0300

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
   resources :tags
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root to: 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
